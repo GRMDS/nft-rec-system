11ef3ccf5ce7ae666c9f1f08d00a8cef4af32824
IsamuYun <yunchunnan@qq.com>
2022-09-08 11:17:52 -0700

    Add 2 data access functions

--- build.py
+++ build.py
@@ -15,11 +15,32 @@ grmds_db_config = {
     'database': 'grmds054_druptest'
 }
 
-def get_nft_items():
+# Get the NFT Category List
+def get_nft_category_list():
+    cursor = nft_conn.cursor()
+
+    select_query = ("SELECT id, name FROM fc_nft_category WHERE deleted = 0")
+    cursor.execute(select_query)
+    rows = cursor.fetchall()
+
+    category_list = []
+    
+    for row in rows:
+        category = {}
+        category["id"] = row[0]
+        category["name"] = row[1]
+        category_list.append(category)
+    
+    cursor.close()
+
+    return category_list
+
+def get_nft_items_by_category(category_id):
     cursor = nft_conn.cursor()
 
     select_query = ("SELECT nft_id, name, description, price, usdt_price, quantity, \
-                    category_id, token_id FROM fc_nft_items WHERE onsell = 1 limit 10")
+                    category_id, token_id FROM fc_nft_items WHERE onsell = 1 AND category_id = %d limit 3" 
+                    % (category_id))
     cursor.execute(select_query)
     rows = cursor.fetchall()
 
@@ -27,15 +48,21 @@ def get_nft_items():
         item = {}
         item["nft_id"] = row[0]
         item["name"] = row[1]
-        item["description"] = row[2]
-        item["price"] = row[3]
-        item["usdt_price"] = row[4]
+        #item["description"] = row[2]
+        #item["price"] = row[3]
+        #item["usdt_price"] = row[4]
         item["quantity"] = row[5]
         item["category_id"] = row[6]
-        item["token_id"] = row[7]
+        #item["token_id"] = row[7]
         print(item)
     
     cursor.close()
+
+def get_nft_items():
+    category_list = get_nft_category_list()
+    print(category_list)
+    for category in category_list:
+        get_nft_items_by_category(category["id"])
 
 if __name__ == "__main__":
     nft_conn = pymysql.connect(**nft_db_config)
